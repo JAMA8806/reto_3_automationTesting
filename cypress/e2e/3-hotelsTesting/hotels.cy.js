18382c03a12a05862d25f2e0241fd3497f97ab0f
JAMA8806 <julianb.e8806@gmail.com>
2023-08-09 19:10:13 -0500

    The cypress project was set up, and the file hotels.cy.js in the cypres/e2e folder was written to test the challenge #3 frontEnd part

---
+++ cypress/e2e/3-hotelsTesting/hotels.cy.js
@@ -0,0 +1,41 @@
+
+describe("Testing", ()=>{
+
+    beforeEach(()=>{
+        
+        cy.visit("http://127.0.0.1:50156/hotels.html")
+    })
+    
+    
+
+    it("Verify all the hotel cards are loaded", ()=>{
+        cy.get('.hotel-card').should('have.length', 18)
+    })
+    
+    it("Verify every price filter match cards", ()=>{
+        
+        cy.get('.filter-prices').select("All prices")
+        cy.get('.hotel-card').should('have.length', 18)
+        
+        cy.get('.filter-prices').select("1")
+        cy.get('div[data-price="1"]').should('have.length', 3)
+        cy.get('.filter-prices').select("2")
+        cy.get('div[data-price="2"]').should('have.length', 4)
+        cy.get('.filter-prices').select("3")
+        cy.get('div[data-price="3"]').should('have.length', 2)
+        cy.get('.filter-prices').select("4")
+        cy.get('div[data-price="4"]').should('have.length', 9)
+        
+        
+    })
+    
+    it("Verify the clear button is working", ()=>{
+        
+        cy.get('.filter-prices').select("4")
+        cy.get('div[data-price="4"]').should('have.length', 9)
+        cy.get('.clear-btn').click()
+        cy.get('.hotel-card').should('have.length', 18)
+
+    })
+    
+})
